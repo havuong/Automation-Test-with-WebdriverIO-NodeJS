6d2ff8b661de1a95d010b662531813630d94235d
havuong <havuong138@gmail.com>
2019-06-25 13:47:48 +0700

    [Mocha Framework] beforeEach and afterEach

--- src/test_scripts/LoginFuntion.js
+++ src/test_scripts/LoginFuntion.js
@@ -3,10 +3,16 @@ let LoginPage = require('../page_objects/LoginPage_Type_3');
 describe('Login Funtion', () => {
 
     before(() => {
+        console.log('\tOpen Testing Page');
         LoginPage.openTestingPage('/login');
     });
 
+    beforeEach(() => {
+        console.log('\tBefore Script');
+    });
+
     it('Login Failed', () => {
+        console.log('\tTestcase 1: Login Failed');
         LoginPage
             .inputUsername('Hav')
             .inputPassword('123456')
@@ -14,11 +20,16 @@ describe('Login Funtion', () => {
     });
 
     it('Login Passed', () => {
+        console.log('\tTestcase 2: Login Passed');
         browser.url('/login')
         LoginPage
             .inputUsername('tomsmith')
             .inputPassword('SuperSecretPassword!')
             .clickOnLoginBtn();
+    });
+
+    afterEach(() => {
+        console.log('\tAfter Script')
     });
 
     after(() => {
